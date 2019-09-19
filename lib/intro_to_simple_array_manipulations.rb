3c99535a3791634d31e06e73fb0431b2c89df1ba
Hamidou Diallo <dio.hamidou@gmail.com>
2019-09-19 19:15:19 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,13 @@
+def using_push(arr,item)
+  return arr.push(item)
+end
+
+def using_unshift(arr,item)
+  return arr.unshift(item)
+end
+
+def using_pop(arr)
+  return arr.pop
+end
+
+def pop_with_args(arr)
